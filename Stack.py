789edfbb55a0e7bdff6a00e2ad9d16d393995e20
Simran Barve <simranbarve@Simrans-MacBook-Air.local>
2020-10-01 08:04:55 +0400

    Def init added

--- Stack.py
+++ Stack.py
@@ -1,24 +1,49 @@
-class Stack
-stack = []
-def push(stack, input):
-    stack = stack.append(input)
-    print(input, "pushed to stack")
+class Stack:
+    stack = []
+    def __init__(self, stack_length = 5):
+        #attributes
+        self.__stack_length = stack_length
 
-def pop(stack):
-    length = len(stack)
-    if length == 0:
-        print("stack is empty")
-    else:
-        popped = (stack[length-1])
-        stack.pop(length - 1)
+    #getters
+    def get_stack_length(self):
+        return self.__stack_length
+
+
+    def push(stack, input):
         print(stack)
-        return(popped)
+        stack = stack.append(input)
+        print(input, "pushed to stack")
 
-def peek(stack):
-    length = len(stack)
-    if length == 0:
-        print("stack is empty")
-    else:
-        print(stack[length-1])
+    def pop(stack):
+        length = len(stack)
+        if length == 0:
+            print("stack is empty")
+        else:
+            popped = (stack[length-1])
+            stack.pop(length - 1)
+            print(stack)
+            return(popped)
 
-push(stack, 1)
+    def peek(stack):
+        length = len(stack)
+        if length == 0:
+            print("stack is empty")
+        else:
+            print(stack[length-1])
+        return length
+
+    def isFull(self):
+        length = len(stack)
+        if length == self.__stack_length:
+            print("stack is full")
+        return length
+
+    def isEmpty(self):
+        length = len(stack)
+        if length == 0:
+            print("stack is empty")
+        return length
+
+Stack1 = Stack(6)
+print(Stack1.get_stack_length())
+print(Stack1.push(3))
