eac2857917a005953b05035b018f491547d65f1b
Simran Barve <simranbarve@Simrans-MacBook-Air.local>
2020-10-11 10:44:52 +0400

    add in the pointer

--- Stack.py
+++ Stack.py
@@ -1,48 +1,48 @@
 class Stack:
-    stack = []
     def __init__(self, stack_length = 5):
         #attributes
-        self.__stack_length = stack_length
+        self.stack_length = stack_length
+        self.stack = [None] * stack_length
+        self.rear = -1
+        self.front = -1
 
-    #getters
-    def get_stack_length(self):
-        return self.__stack_length
+
 
 
     def push(stack, input):
-        print(stack)
-        stack = stack.append(input)
-        print(input, "pushed to stack")
+        if not self.isFull():
+            if self.isEmpty():
+                self.rear = 0
+                self.front = 0
+            else:
+                self.rear += 1
+            self.stack[self.rear] = input
+            return True
+        return False
 
     def pop(stack):
-        length = len(stack)
-        if length == 0:
-            print("stack is empty")
+        if not self.isFull():
+            popped = (stack[rear])
+            stack.pop(rear)
+            return (popped)
         else:
-            popped = (stack[length-1])
-            stack.pop(length - 1)
-            print(stack)
-            return(popped)
+            return False
 
     def peek(stack):
-        length = len(stack)
-        if length == 0:
-            print("stack is empty")
-        else:
-            print(stack[length-1])
-        return length
+        if not self.isEmpty():
+            print(stack[rear])
+            return True
+        return False
 
     def isFull(self):
-        length = len(stack)
-        if length == self.__stack_length:
-            print("stack is full")
-        return length
+        if self.rear == self.stack_length:
+            return True
+        return False
 
     def isEmpty(self):
-        length = len(stack)
-        if length == 0:
-            print("stack is empty")
-        return length
+        if self.rear and self.front == -1:
+            return True
+        return False
 
 Stack1 = Stack(6)
 print(Stack1.get_stack_length())
