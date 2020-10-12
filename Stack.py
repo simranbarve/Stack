1dfba39e42cbc1944c1b54b2f78f49d40b25486c
Simran Barve <simranbarve@Simrans-MacBook-Air.local>
2020-10-12 18:03:41 +0400

    completed all edits with pointer implemented

--- Stack.py
+++ Stack.py
@@ -3,47 +3,59 @@ class Stack:
         #attributes
         self.stack_length = stack_length
         self.stack = [None] * stack_length
-        self.rear = -1
-        self.front = -1
-
-
-
-
-    def push(stack, input):
-        if not self.isFull():
-            if self.isEmpty():
-                self.rear = 0
-                self.front = 0
-            else:
-                self.rear += 1
-            self.stack[self.rear] = input
-            return True
-        return False
-
-    def pop(stack):
-        if not self.isFull():
-            popped = (stack[rear])
-            stack.pop(rear)
-            return (popped)
-        else:
-            return False
-
-    def peek(stack):
-        if not self.isEmpty():
-            print(stack[rear])
-            return True
-        return False
+        self.pointer = -1
 
     def isFull(self):
-        if self.rear == self.stack_length:
+        if self.pointer == (self.stack_length - 1):
             return True
         return False
 
     def isEmpty(self):
-        if self.rear and self.front == -1:
+        if self.pointer == -1:
+            return True
+        return False
+
+    def push(self, input):
+        if not self.isFull():
+            self.pointer += 1
+            self.stack[self.pointer] = input
+            return True
+        else:
+            print("stack is full")
+            return False
+
+    def pop(self):
+        if not self.isEmpty():
+            popped = (stack[rear])
+            stack.pop(rear)
+            return (popped)
+        else:
+            print("stack is empty")
+            return False
+
+    def peek(self):
+        if not self.isEmpty():
+            print(stack[rear])
+            return True
+        print("stack is empty")
+        return False
+
+
+
+    def printing(self):
+        if not self.isEmpty():
+            print(self.stack)
             return True
         return False
 
 Stack1 = Stack(6)
-print(Stack1.get_stack_length())
-print(Stack1.push(3))
+Stack1.pop()
+Stack1.push(3)
+Stack1.push(2)
+Stack1.push(1)
+Stack1.push(6)
+Stack1.push(5)
+Stack1.printing()
+Stack1.push(7)
+Stack1.push(4)
+Stack1.printing()
